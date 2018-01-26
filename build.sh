27b8f05b4b147daea2c1780183f139c4631f65c8
Gabor Heja <gheja@m002560p>
2018-01-26 23:54:10 +0100

    Added prechecks for builders in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+./builders/android-builder/precheck.sh
+./builders/nwjs-builder/precheck.sh
+./builders/zip-builder/precheck.sh
+
 ./builders/android-builder/build.sh ./src ./dist test-project 0.1.0
 ./builders/nwjs-builder/build.sh ./src ./dist test-project 0.1.0
 ./builders/zip-builder/build.sh ./src ./dist test-project 0.1.0
